b6b334f11c1029212745c377aad6b7fbafb20ab7
Dominik Kaliszewski <kaliszewski.dominik@gmail.com>
2021-10-03 13:24:03 +0200

    Init project

---
+++ src/main/java/com/kaliszewski/datarelations/DatarelationsApplication.java
@@ -0,0 +1,13 @@
+package com.kaliszewski.datarelations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatarelationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatarelationsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaliszewski/datarelations/DatarelationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaliszewski.datarelations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatarelationsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
